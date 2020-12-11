759de5d3ba48248c7d548397d170a01a6f8e97b9
kanuchang <lin.f.zhang@gmail.com>
2020-12-11 12:38:46 +0800

    Add files via upload

--- bin/__init__.py
+++ bin/__init__.py
@@ -1,1 +1,8 @@
-z
+#!/usr/bin/env python3
+# _*_ coding:utf-8 _*_
+
+# @Time    : 2020/11/29 17:07
+# @Author  : TNanko
+# @Site    : https://tnanko.github.io
+# @File    : __init__.py.py
+# @Software: PyCharm
